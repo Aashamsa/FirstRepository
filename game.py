6ab85a7e1163580bc285dff2f22275a23fabedc2
Aashamsa <baashamsa@gmail.com>
2024-05-18 12:47:34 +0545

    Game

--- game.py
+++ game.py
@@ -1,19 +1,17 @@
-import pygame.display
 import pygame
-pygame.init()
-screen = pygame.display.set_mode((800,600))
-pygame.display.set_caption("My Game")
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+from pygame.locals import *
+
+resolution = (400,300)
+white = (255,255,255)
+black = (0,0,0)
+red = (255,0,0)
+
+screen = pygame.display.set_mode(resolution)
+
+while True:
+    screen.fill(white)
     pygame.display.flip()
-pygame.quit()
-for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-        running = False
-    elif event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_SPACE:
-            running = False
-             
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
