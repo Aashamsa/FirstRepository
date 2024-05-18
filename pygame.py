42b66d1647b8c404664d74282a00406301691327
Aashamsa <baashamsa@gmail.com>
2024-05-18 11:59:30 +0545

    Pygame code (1st)

--- pygame.py
+++ pygame.py
@@ -1,32 +1,63 @@
+# import the pygame module
 import pygame
+
+# import pygame.locals for easier 
+# access to key coordinates
 from pygame.locals import *
 
-class square(pygame.sprite.Sprite):
-    def __init__(self):
-        super(square,self).__init__()
-        self.surf = pygame.surface((25,25))
-        self.surf.fill((0,200,225))
-        self.rect = self.surf.get_rect()
+# Define our square object and call super to
+# give it all the properties and methods of pygame.sprite.Sprite
+# Define the class for our square objects
+class Square(pygame.sprite.Sprite):
+	def __init__(self):
+		super(Square, self).__init__()
+		
+		# Define the dimension of the surface
+		# Here we are making squares of side 25px
+		self.surf = pygame.Surface((25, 25))
+		
+		# Define the color of the surface using RGB color coding.
+		self.surf.fill((0, 200, 255))
+		self.rect = self.surf.get_rect()
 
+# initialize pygame
 pygame.init()
-screen =pygame.display.set_mode((800,600))
-square1=square()
-square2=square()
-square3=square()
-square4=square()
 
-gameon = True
+# Define the dimensions of screen object
+screen = pygame.display.set_mode((800, 600))
 
-while gameon:
-    for x in pygame.x.get():
-        if x.type == KEYDOWN:
-            if x.key == K_BACKSPACE:
-                gameon = False
-        elif x.type == QUIT:
-            gameon = False
-    screen.blit(square1.surf(40,40))
-    screen.blit(square2.surf(80,80))
-    screen.blit(square3.surf(160,160))
-    screen.blit(square4.surf(240,240))
+# instantiate all square objects
+square1 = Square()
+square2 = Square()
+square3 = Square()
+square4 = Square()
 
-    pygame.display.flip()
+# Variable to keep our game loop running
+gameOn = True
+
+# Our game loop
+while gameOn:
+	# for loop through the event queue
+	for event in pygame.event.get():
+		
+		# Check for KEYDOWN event
+		if event.type == KEYDOWN:
+			
+			# If the Backspace key has been pressed set
+			# running to false to exit the main loop
+			if event.key == K_BACKSPACE:
+				gameOn = False
+				
+		# Check for QUIT event
+		elif event.type == QUIT:
+			gameOn = False
+
+	# Define where the squares will appear on the screen
+	# Use blit to draw them on the screen surface
+	screen.blit(square1.surf, (40, 40))
+	screen.blit(square2.surf, (40, 530))
+	screen.blit(square3.surf, (730, 40))
+	screen.blit(square4.surf, (730, 530))
+
+	# Update the display using flip
+	pygame.display.flip()
