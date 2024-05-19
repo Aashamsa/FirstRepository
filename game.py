0db749aea572ecf030259646b3f395a804efb0be
Aashamsa <baashamsa@gmail.com>
2024-05-19 10:33:01 +0545

    Game Example

--- game.py
+++ game.py
@@ -1,17 +1,25 @@
-import pygame
-from pygame.locals import *
-
-resolution = (400,300)
-white = (255,255,255)
-black = (0,0,0)
-red = (255,0,0)
-
-screen = pygame.display.set_mode(resolution)
-
-while True:
-    screen.fill(white)
-    pygame.display.flip()
-
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
+# import pygame package 
+import pygame 
+  
+# initializing imported module 
+pygame.init() 
+  
+# displaying a window of height 
+# 500 and width 400 
+pygame.display.set_mode((400, 500)) 
+  
+# creating a bool value which checks 
+# if game is running 
+running = True
+  
+# keep game running till running is true 
+while running: 
+      
+    # Check for event if user has pushed 
+    # any event in queue 
+    for event in pygame.event.get(): 
+          
+        # if event is of type quit then  
+        # set running bool to false 
+        if event.type == pygame.QUIT: 
+            running = False
